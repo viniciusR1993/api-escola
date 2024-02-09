6a4409fb13e52e569864646f2a91cf4276ac54f6
Vinicius Ramalho Araujo <60826701+viniciusR1993@users.noreply.github.com>
2024-02-09 09:27:48 -0300

    Adiciona host

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'we2q@@2p30+=^#(xbgl7e@3+r2dt+zmf#us2m$ix=q&n+=y-z$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.222.135.38']
 
 
 # Application definition
